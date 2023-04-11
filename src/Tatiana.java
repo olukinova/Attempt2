28c0449653467da6fffaac6c31a9b64a155c4303
tatiana25551 <t.fedorenco@gmail.com>
2023-04-10 21:42:59 -0400

    code added

---
+++ src/Tatiana.java
@@ -0,0 +1,9 @@
+public class Tatiana {
+    public static void main(String[] args) {
+
+        System.out.println("Attempt nr 2 is successful");
+
+        System.out.println("I love summer");
+    }
+
+}
