92465d8cdffd418e7f2af1bd7126a03fb5dcf85c
tatiana25551 <t.fedorenco@gmail.com>
2023-04-10 21:55:22 -0400

    main class

--- src/Tatiana.java
+++
@@ -1,9 +0,0 @@
-public class Tatiana {
-    public static void main(String[] args) {
-
-        System.out.println("Attempt nr 2 is successful");
-
-        System.out.println("I love summer");
-    }
-
-}
